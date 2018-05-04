45f373a699ce03e5e80dc9fe1dbe16d523db155c
wing <wng2014@gmail.com>
2018-05-03 19:21:12 -0700

    redo last commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,29 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView maka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +69,25 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        /*
+        <item>{\"name\":{\"mainName\":\"Ham and cheese sandwich\",\"alsoKnownAs\":[]},\"placeOfOrigin\":\"\",\"description\":\"A ham and cheese
+        sandwich is a common type of sandwich. It is made by putting cheese and sliced ham
+        between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables
+        like lettuce, tomato, onion or pickle slices can also be included. Various kinds of
+        mustard and mayonnaise are also
+        common.\",\"image\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG\",\"ingredients\":[\"Sliced
+        bread\",\"Cheese\",\"Ham\"]}
+        maka = (TextView) findViewById(R.id.aka_tv);
+        */
+        try {
+            JSONArray nameArray = sandwich.getJSONArray("name");
+            JSONObject nameObject = nameArray.getJSONObject(0);
+            String mainName = nameObject.getString("mainName");
+            String aka = nameObject.getString("alsoKnownAs");
+            maka.append(aka);
+        }catch (exception e) {
+            closeOnError();
+        }
 
     }
 }
