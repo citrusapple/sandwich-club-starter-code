3b665f49ed295546f46ebf15b3289eb2e3ec9f18
wing <wng2014@gmail.com>
2018-05-09 21:37:49 -0700

    added .placeholder with Picasso for data without valid image

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -57,8 +57,10 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI(sandwich);
+
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.mipmap.ic_launcher)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
